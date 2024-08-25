6f7cab0e938d22524fcd97c8f40c261775ee7f7b
adarshcoders <110824442+adarshcoders@users.noreply.github.com>
2024-08-25 18:48:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Made By Animecx","Scanning Bots"];
 
 
 let currentIndex = 0;
